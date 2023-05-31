90610d17bd8fcc7abdd6d56aae2bcc3754c8f433
zangar1 <zangarbatyrkhan@gmail.com>
2023-05-31 16:51:24 +0600

    added3>2

--- src/test/java/one/tech/exampleTest.java
+++ src/test/java/one/tech/exampleTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 public class exampleTest {
     @Test
     void exampleTest() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2 > 1);
     }
-}
+        @Test
+        void exampleTest1() {
+            Assertions.assertTrue(3 > 2);
+        }
+    }
+
