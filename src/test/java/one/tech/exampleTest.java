98fb206db8feca725890e4884328bef64ac2c74e
zangar1 <zangarbatyrkhan@gmail.com>
2023-05-31 16:13:03 +0600

    init was added and src test

---
+++ src/test/java/one/tech/exampleTest.java
@@ -0,0 +1,11 @@
+package one.tech;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTest {
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue(2>1);
+    }
+}
